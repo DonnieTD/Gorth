4dd5b9d08d720588ebe89fa2c4a9ce72ff74076c
DonnieTD <donniethedev@gmail.com>
2022-06-25 15:03:06 +0000

    finished asm for the ops left over in the example hardcoded program

--- main.go
+++ main.go
@@ -24,15 +24,27 @@ func main() {
 		fmt.Println("")
 		fmt.Println("ERROR: no subcommand was provided")
 		fmt.Println("")
-		utils.Usage()
+		utils.Usage(os.Args[0])
 		os.Exit(1)
 	}
 
 	subcommand := os.Args[1]
 
 	if subcommand == "sim" {
+		if len(os.Args) < 3 {
+			fmt.Println("")
+			fmt.Println("ERROR: no path was provided to sim")
+			fmt.Println("")
+			os.Exit(1)
+		}
 		SimulateProgram(Program)
 	} else if subcommand == "com" {
+		if len(os.Args) < 3 {
+			fmt.Println("")
+			fmt.Println("ERROR: no path was provided to com")
+			fmt.Println("")
+			os.Exit(1)
+		}
 		CompileProgram(Program, "output.asm")
 		utils.RunCMD("nasm -felf64 output.asm")
 		utils.RunCMD("ld -o output output.o")
